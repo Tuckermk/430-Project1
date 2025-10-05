d1777032361d0b29456568c630cfc70d25cd459a
Tucker <tmk1270@g.rit.edu>
2025-10-05 12:52:53 -0400

    structured Gets, the pathing to be used, and htmlResponse

---
+++ src/server.js
@@ -0,0 +1,70 @@
+const http = require('http');
+const query = require('querystring');
+const htmlHandler = require('./htmlResponse.js');
+const jsonHandler = require('./jsonResponse.js');
+
+const port = process.env.PORT || process.env.NODE_PORT || 3000;
+
+//handle incoming data to make it usable
+const parseBody = (request, response, handler) => {
+  const body = [];
+  request.on('error', () => {
+    response.statusCode = 400;
+    response.end();
+  });
+  request.on('data', (chunk) => {
+    body.push(chunk);
+  });
+  request.on('end', () => {
+    const bodyString = Buffer.concat(body).toString();
+    request.body = query.parse(bodyString);
+    handler(request, response);
+  });
+};
+
+// handle POST requests
+const handlePost = (request, response, parsedUrl) => {
+//   if (parsedUrl.pathname === '/addUser') {
+//     parseBody(request, response, jsonHandler.addUsers);
+//   }
+};
+
+//All possible request pathes the server will use
+const pathes = {
+   //html pathes
+   '/': htmlHandler.getIndex,
+   '/style.css': htmlHandler.getCSS,
+   //json pathes
+
+
+   //error pathes
+  '/badRequest': jsonHandler.badRequest,
+  '/unauthorized': jsonHandler.unauthorized,
+  '/forbidden': jsonHandler.forbidden,
+  '/internal': jsonHandler.internal,
+  '/notImplemented': jsonHandler.notImplemented,
+  notfound: jsonHandler.notFound,   
+}
+
+// handle GET requests
+const handleGet = (request, response, parsedUrl) => {
+   if(pathes[parsedUrl.pathname]){
+      pathes[parsedUrl.pathname](request,response);
+   }
+   else {
+      pathes.notFound(request,response);
+   }
+
+};
+
+const onRequest = (request, response) => {
+  const protocol = request.connection.encrypted ? 'https' : 'http';
+  const parsedUrl = new URL(request.url, `${protocol}://${request.headers.host}`);
+  if (request.method === 'POST') {
+    handlePost(request, response, parsedUrl);
+  } else {
+    handleGet(request, response, parsedUrl);
+  }
+};
+
+http.createServer(onRequest).listen(port);
